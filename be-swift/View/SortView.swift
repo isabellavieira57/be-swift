f892ae5cb1a747f4e7e3e0909286bb130c7f73e9
Ana Carolina <anamuller@anas-macbook-pro.local>
2017-10-18 15:54:35 -0200

    início sortView

--- be-swift/View/SortView.swift
+++ be-swift/View/SortView.swift
@@ -7,7 +7,91 @@
 //
 
 import Foundation
+import UIKit
 
 class SortView: View {
     
+    var sortTableView: UITableView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+//        var screenSize = UIScreen.main.bounds
+//        var screenHeight = screenSize.height
+//        var screenWidth = screenSize.width
+        
+        self.backgroundColor = UIColor.lightGray
+        
+        sortTableView = UITableView(frame: CGRect(x: 0, y: 0, width: 321, height: 400))
+        
+        self.addSubview(sortTableView)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
+
+class SortViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var sortView: SortView!
+    
+    var arrayOptions = ["line 1", "line 2", "line 3", "line 4", "line 5"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.sortView.sortTableView.isEditing = true
+        
+        self.sortView = SortView()
+        self.view = self.sortView
+        
+        self.sortView.sortTableView.dataSource = self
+        self.sortView.sortTableView.delegate = self
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return arrayOptions.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        
+        cell?.textLabel?.text = arrayOptions[indexPath.row]
+        
+        return cell!
+    }
+    
+    //Enables rearrengement of lines
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    //Rearrenges lines
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        
+        let rearrangedLine = arrayOptions[sourceIndexPath.row]
+        arrayOptions.remove(at: sourceIndexPath.row)
+        arrayOptions.insert(rearrangedLine, at: destinationIndexPath.row)
+        
+        NSLog("%@", "\(sourceIndexPath.row) => \(destinationIndexPath.row) \(arrayOptions)")
+        self.sortView.sortTableView.reloadData()
+    }
+    
+    //Disable delete buttons when editing the order of cells
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        return .none
+    }
+    
+    func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    
+}
+
