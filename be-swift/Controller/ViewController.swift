38c5b1ba538dfd030f4d2b90ffcf73ba10cfd1a5
isabellavieira57 <isabellavieira57@gmail.com>
2017-11-07 18:01:19 -0200

    movendo handler e acesso ao banco da levelcontroller para viewcontroller

--- be-swift/Controller/ViewController.swift
+++ be-swift/Controller/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 import Firebase
 import FirebaseDatabase
 
-class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+protocol LevelHandler {
+    func getLevelData (level: Level)
+}
+
+class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource, LevelHandler {
     
     var challengesView: CollectionChallengeView!
     var arrayChallengeInfo = [UserChallengeInfo]()
     var arrayChallenges = [Challenge]()
     var user: User!
     
-    override func viewDidLoad()
-    {
+    override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print ("GET CHALLENGE")
+        let levelDAO = LevelDAO()
+        
+        //getChallengesByLevel results are in the handler getLevelData
+        levelDAO.getChallengesByLevel(handler: self, level: "level-1")
         
         self.challengesView = CollectionChallengeView()
         self.view.addSubview(challengesView)
@@ -33,13 +42,11 @@ class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICo
 //        self.challengesView.xpLabel.text = user.xpTotal
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
-    {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 12
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
-    {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = challengesView.collectionChallenges1.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath as IndexPath) as! CollectionChallengesCell
         
         let stars = 2
@@ -52,32 +59,31 @@ class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICo
         return cell
     }
     
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath)
-    {
-        var mechanics = "MultipleChoice"
-//        var mechanics = self.arrayChallenges[indexPath.item].mechanics
-        var nextController: UIViewController!
-        
-//        if arrayChallengeInfo id = self.arrayChallenges[indexPath.item].challengeID {
-        
-        switch mechanics 
-      {
-       case "BlankField":
-             nextController = BlankFieldViewController()
-        //        case "DragAndDrop":
-        //            nextController = DragAndDropController()
-        case "MultipleChoice":
-            nextController = MultipleChoiceController()
-        case "Sort":
-            nextController = SortViewController()
-        //        case "FillTheGap":
-        //            nextController = FillTheGapController()
-        default:
-            nextController = self
-        }
-        
-        present(nextController, animated: true, completion: nil)
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        var mechanics = "MultipleChoice"
+////        var mechanics = self.arrayChallenges[indexPath.item].mechanics
+//        var nextController: UIViewController!
+//
+////        if arrayChallengeInfo id = self.arrayChallenges[indexPath.item].challengeID {
+//
+//        switch mechanics
+//      {
+//       case "BlankField":
+//             nextController = BlankFieldViewController()
+//        //        case "DragAndDrop":
+//        //            nextController = DragAndDropController()
+//        case "MultipleChoice":
+//            nextController = MultipleChoiceController()
+//        case "Sort":
+//            nextController = SortViewController()
+//        //        case "FillTheGap":
+//        //            nextController = FillTheGapController()
+//        default:
+//            nextController = self
 //        }
+//
+//        present(nextController, animated: true, completion: nil)
+////        }
     }
         
 //    override func viewDidAppear(_ animated: Bool) {
@@ -97,6 +103,30 @@ class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICo
     //        let webView = WebDocumentationViewController()
     //        present(webView, animated: false, completion: nil)
     //    }
+    
+    // This function gets the return of the Firebase asynchronous call and call the respective view
+    func getLevelData(level: Level) {
+        print ("HANDLER LEVEL ASSINCRONO")
+        
+        for challenge in level.challenge {
+            //print (">>>>> question: \(challenge.question)")
+            
+            if (challenge.mechanics == "MultipleChoice") {
+                // TODO: Build view for multiple choice
+            } else if (challenge.mechanics == "DragAndDrop") {
+                // TODO: Build view for drag and drop
+            } else if (challenge.mechanics == "BlankField") {
+                // TODO: Build view for blank field
+            } else if (challenge.mechanics == "FillTheGap") {
+                // TODO: Build view for fill the gap
+            } else if (challenge.mechanics == "Sort") {
+                // TODO: Build view for sort
+            } else {
+                print ("ERROR: Mechanics not found!")
+            }
+            
+        }
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
